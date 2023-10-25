06d9a3fa6849a452bbc33b5b741b4058fd9e244b
Georgios Vlassis <gvlassis@mailbox.org>
2023-10-25 19:05:31 +0200

    Syntax highlighting, bash, true and false

--- src/vscode.py
+++ src/vscode.py
@@ -240,7 +240,7 @@ def write(file, mode, scheme, scheme_8):
                 "settings": {"foreground": mcu.hexFromArgb(scheme_8.get_green())}
             },
             {
-                "scope": "string, constant.numeric.integer.shell",
+                "scope": "string, constant.numeric.integer.shell, constant.language.false.shell, constant.language.true.shell",
                 "settings": {"foreground": mcu.hexFromArgb(scheme_8.get_yellow())}
             },
             {
